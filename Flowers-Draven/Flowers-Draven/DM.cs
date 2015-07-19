a36971e5cfead83fc572172cbdb613348e778eb7
Nightmoon <1076751236@qq.com>
2015-07-19 20:46:47 +0800

    Removed GitHub :https://github.com/1076751236/Flowers

---
+++ Flowers-Draven/Flowers-Draven/DM.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+namespace Flowers_Draven
+{
+    class DM
+    {
+        public static Menu 菜单;
+        public static void DMenu()
+        {
+            菜单 = new Menu("Flowers-Draven", "Feeeez", true);
+
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            TargetSelector.AddToMenu(targetSelectorMenu);
+            菜单.AddSubMenu(targetSelectorMenu);
+
+            var orbwalkMenu = new Menu("Orbwalker", "orbwalker");
+            Draven.Orbwalker = new Orbwalking.Orbwalker(orbwalkMenu);
+            菜单.AddSubMenu(orbwalkMenu);
+
+            菜单.AddSubMenu(new Menu("Combo", "Combo"));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzq", "Use Q").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzw", "Use W").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzw1", "Use W Dash").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lze", "Use E").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzr", "Use R").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzr1", "Use R2").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzrhp", "Use R Emery Hp <=").SetValue(new Slider(50, 0, 100)));
+
+            菜单.AddSubMenu(new Menu("Harass", "Harass"));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srq", "Use Q").SetValue(true));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srw", "Use W").SetValue(true));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srw1", "Use W Dash").SetValue(true));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("sre", "Use E").SetValue(true));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srmp", "Harass Mana <=").SetValue(new Slider(40, 0, 100)));
+
+            菜单.AddSubMenu(new Menu("Clear", "Clear"));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qx", "LaneClear"));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxq", "Use Q").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qy", "JungleClear"));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qyq", "Use Q").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qyw", "Use W").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qye", "Use E").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxmp", "Clear Mana <=").SetValue(new Slider(20, 0, 100)));
+
+            菜单.AddSubMenu(new Menu("AxeSetting", "axeSetting"));
+            菜单.SubMenu("axeSetting").AddItem(
+                new MenuItem("AxeMode", "Axe Mode:").SetValue(new StringList(new[] {"Combo", "Ramdom", "Always"},
+                    2)));
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("DrawAxeLocation", "Draw Axe Location").SetValue(true));
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("DrawAxeRange", "Draw Axe Range").SetValue(true));
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("CatchAxeRange", "Catch Axe Range").SetValue(new Slider(800, 120, 1500)));
+
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("MaxAxes", "Max Axes").SetValue(new Slider(2, 1, 3)));
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("UseWForQ", "Use W For Q").SetValue(true));
+            菜单.SubMenu("axeSetting").AddItem(new MenuItem("DontCatchUnderTurret", "Dont Catch Under Turret").SetValue(true));
+
+            菜单.AddSubMenu(new Menu("Item", "Item"));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseYUU", "UseYUU").SetValue(true));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseBRK", "UseBRK").SetValue(true));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseBC", "UseBC").SetValue(true));
+
+            菜单.AddSubMenu(new Menu("Drawing", "Drawing"));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawing", "Dis drawing").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingQ", "Q Range").SetValue(new Circle(true, Color.FromArgb(138, 101, 255))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingE", "E Range").SetValue(new Circle(true, Color.FromArgb(255, 0, 0))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingR", "R Range").SetValue(new Circle(false, Color.FromArgb(0, 255, 0))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("bdxb", "Million LastHit").SetValue(new Circle(true, Color.GreenYellow)));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("fjkjs", "Millions Can Kill").SetValue(new Circle(true, Color.Gray)));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingAA", "AA Range(OKTW© Style)").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("orb", "AA Target(OKTW© Style)").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("wushangdaye", "Jungle Postion").SetValue(true));
+
+            菜单.AddToMainMenu();
+        }
+    }
+}
---
+++ Flowers-Kalista/Flowers-Kalista/KalistaM.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+namespace Flowers滑板鞋_重生_
+{
+    class KalistaM
+    {
+        public static Menu 菜单;
+        public static void KalistaMenu()
+        {
+            菜单 = new Menu("Flowers-Kalista", "Lost.", true);
+
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            TargetSelector.AddToMenu(targetSelectorMenu);
+            菜单.AddSubMenu(targetSelectorMenu);
+
+            var orbwalkerMenu = new Menu("Orbwalker", "Orbwalker");
+            lost.Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+            菜单.AddSubMenu(orbwalkerMenu);
+
+            菜单.AddSubMenu(new Menu("Combo", "Combo"));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzp", "Use Q").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lze", "Use E").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzeee", "Use E KS").SetValue(true));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzeeeeee", "Max E Stack").SetValue(new Slider(5, 1, 20)));
+            菜单.SubMenu("Combo").AddItem(new MenuItem("lzmp", "Combo Mana <=%").SetValue(new Slider(50, 0, 100)));
+
+            菜单.AddSubMenu(new Menu("Harass", "Harass"));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srq", "Use Q").SetValue(true));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("AutoQ", "Auto Harass").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Toggle)));
+            菜单.SubMenu("Harass").AddItem(new MenuItem("srmp", "Harass Mana <=%").SetValue(new Slider(50, 0, 100)));
+
+            菜单.AddSubMenu(new Menu("Clear", "Clear"));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxq", "Use Q LaneClear").SetValue(false));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxqqq", "Use Q Millions").SetValue(new Slider(3, 1, 5)));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxe", "Use E LaneClear").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxeee", "Use E Millions").SetValue(new Slider(2, 1, 5)));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qyq", "Use Q JungleClear").SetValue(false));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qye", "Use E JungleClear").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("eqiangyeguai", "Use E Steal Jungle").SetValue(true));
+            菜单.SubMenu("Clear").AddItem(new MenuItem("qxmp", "Clear Mana <=%").SetValue(new Slider(60, 0, 100)));
+
+            /*{        
+            
+            
+            菜单.AddSubMenu(new Menu("R Setting", "RSetting"));
+            菜单.SubMenu("RSetting").AddItem(new MenuItem("jilao", "Save Your Sweetheart").SetValue(true));
+            菜单.SubMenu("RSetting").AddItem(new MenuItem("jilaohp", "Sweetheart Hp <=%").SetValue(new Slider(20, 0, 100)));
+                         }*/
+            菜单.AddSubMenu(new Menu("Item", "Item"));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseYUU", "Use Youmuu's Ghostblade").SetValue(true));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseBRK", "Use Blade of the Ruined King").SetValue(true));
+            菜单.SubMenu("Item").AddItem(new MenuItem("UseBC", "Use Bilgewater Cutlass").SetValue(true));
+
+            菜单.AddSubMenu(new Menu("Flee", "Flee"));
+            菜单.SubMenu("Flee").AddItem(new MenuItem("Flee", "Flee").SetValue(new KeyBind("Z".ToCharArray()[0], KeyBindType.Press)));            
+
+
+         /*   菜单.AddSubMenu(new Menu("Balista", "BiuBiuBiu"));
+            菜单.SubMenu("BiuBiuBiu").AddItem(new MenuItem("useToggle", "Toggle").SetValue(false));
+            菜单.SubMenu("BiuBiuBiu").AddItem(new MenuItem("useOnComboKey", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
+            菜单.SubMenu("BiuBiuBiu").AddItem(new MenuItem("minRange", "Min Range to Balista", true).SetValue(new Slider(700, 100, 1449)));
+            菜单.SubMenu("BiuBiuBiu").AddItem(new MenuItem("maxRange", "Max Range to Balista", true).SetValue(new Slider(1500, 100, 1500)));
+            菜单.SubMenu("BiuBiuBiu").AddItem(new MenuItem("BiuBiuBiu1", "Kalista and Blitzcrank biubiubiu~"));
+
+            菜单.AddSubMenu(new Menu("Balista Target", "BalistaTarget"));*/
+
+            菜单.AddSubMenu(new Menu("Misc", "Misc"));
+            菜单.SubMenu("Misc").AddItem(new MenuItem("DamageExxx", "Auto E -> When millions will die and hero has buff").SetValue(true));
+            菜单.SubMenu("Misc").AddItem(new MenuItem("usePackets", "Use Packets").SetValue(false));
+
+            菜单.AddSubMenu(new Menu("Drawings", "Drawing"));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingQ", "Q Range").SetValue(new Circle(true, Color.FromArgb(138, 101, 255))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingW", "W Range").SetValue(new Circle(false, Color.FromArgb(202, 170, 255))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingE", "E Range").SetValue(new Circle(true, Color.FromArgb(255, 0, 0))));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingR", "R Range").SetValue(new Circle(false, Color.FromArgb(0, 255, 0))));
+         /*   菜单.SubMenu("Drawing").AddItem(new MenuItem("minBRange", "Balista Min Range").SetValue(new Circle(false, Color.Chartreuse)));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("maxBRange", "Balista Max Range").SetValue(new Circle(false, Color.Green)));*/
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("bdxb", "Draw Minion LastHit").SetValue(new Circle(true, Color.GreenYellow)));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("fjkjs", "Draw Minion Near Kill").SetValue(new Circle(true, Color.Gray)));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("drawingAA", "Real AA Range(OKTW© Style)").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("orb", "AA Target(OKTW© Style)").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("DrawEDamage", "Drawing E Damage").SetValue(true));
+            菜单.SubMenu("Drawing").AddItem(new MenuItem("wushangdaye", "Jungle position").SetValue(true));
+
+            菜单.AddSubMenu(new Menu("Message", "Message"));
+            菜单.SubMenu("Message").AddItem(new MenuItem("Credit", "Credit : NightMoon"));
+            菜单.SubMenu("Message").AddItem(new MenuItem("Version", "Version : 1.0.0.0"));
+            菜单.SubMenu("Message").AddItem(new MenuItem("ttt", "if you have more Bug pls tell me"));
+
+            菜单.AddToMainMenu();
+        }
+    }
+}
