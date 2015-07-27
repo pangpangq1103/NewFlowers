b4bab7223d1ff7208db3f60487c5b6d01ef46696
Nightmoon <1076751236@qq.com>
2015-07-27 20:22:21 +0800

    new prediction it was working~

--- LeagueSharp.Common For CN/Prediction.cs
+++ LeagueSharp.Common For CN/Prediction.cs
@@ -221,14 +221,12 @@ namespace LeagueSharp.Common
             _configMenu = config;
             Notifications.AddNotification("需要充能的技能(有释放延迟)无法使用新预判~~", 4000);
             Notifications.AddNotification("例如泽拉斯无法使用新预判 等待升级~", 4000);
-            var predictionsettings = new MenuItem("预判设置", "predictionsettings", true);
 
             config.AddItem(new MenuItem("method", "预判方式", true).SetShared().SetValue(new StringList(new[] 
             { "新预判(极端)", "新预判(正常)", "原版库预判" }))).
             ValueChanged += Prediction_ValueChanged;
 
-            config.AddItem(predictionsettings);
-            config.AddItem(new MenuItem("Sep1", "123").SetShared());
+            config.AddItem(new MenuItem("Sep1", "花边尝鲜新预判模式").SetShared());
         }
 
         static void Prediction_ValueChanged(object sender, OnValueChangeEventArgs e)
